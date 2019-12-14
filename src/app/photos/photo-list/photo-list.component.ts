032dd142212013795e0a16b41ba4ed5894fabad4
Conrado <conradoms@gmail.com>
2019-12-14 11:07:24 -0300

    Adicionando rotas parametrizadas

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.service';
@@ -12,10 +13,15 @@ export class PhotoListComponent implements OnInit {
   
   photos: Photo[] = [];
 
-  constructor(private photoService: PhotoService){}
+  constructor(
+    private photoService: PhotoService,
+    private activatedRoute: ActivatedRoute
+    ){}
   
   ngOnInit(): void{
-    this.photoService.listFromUser('flavio')
+    
+    const userName = this.activatedRoute.snapshot.params.userName;
+    this.photoService.listFromUser(userName)
       .subscribe(photo => {
         console.log(photo[0].description)
         this.photos = photo
