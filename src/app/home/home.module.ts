dd0004983c53aa1de1d0b92b1c1385fb4fa1c841
Conrado <conradoms@gmail.com>
2019-12-17 19:10:49 -0300

    Implementação da validação dos campos do formulário de login

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule} from '@angular/forms';
 
 import { SignInComponent } from './signin/signin.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-    declarations: [ SignInComponent ]
+    declarations: [ SignInComponent ],
+    imports: [ 
+        ReactiveFormsModule,
+        CommonModule
+     ]
 })
 export class HomeModule{}
